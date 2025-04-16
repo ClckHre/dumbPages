0bc447f49f7e50896faa9d888bf667d91448c00e
ClckHre <iwiswi.albir@gmail.com>
2025-04-16 19:37:39 +0200

    Create brainfuck.js

---
+++ brainfuck/brainfuck.js
@@ -0,0 +1,12 @@
+function interpret(codestring) {
+  code = Array.from(codestring);
+  array = [];
+  amount = 20000;
+  aIndex = 0;
+  cIndex = 0;
+  for (let i = 0; i < amount; i++) {
+    array.push(0);
+  }
+  console.log("beans");
+}
+interpret("+++")
